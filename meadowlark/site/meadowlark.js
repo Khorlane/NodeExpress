b6dd764fd768fea3b551d028f505d7317ae12546
Steve Bryant <khorlane@yahoo.com>
2024-02-01 20:25:12 -0500

    Meadowlark p90 - Form Handling with Express

--- meadowlark/site/meadowlark.js
+++ meadowlark/site/meadowlark.js
@@ -15,6 +15,7 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
+app.use(require('body-parser')());
 
 // Home page
 app.get('/', function(req, res)
@@ -34,6 +35,24 @@ app.get('/headers', function(req,res){
   var s = '';
   for(var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
   res.send(s);
+});
+
+// Email page
+app.get('/newsletter', function(req, res){
+  res.render('newsletter', { csrf: 'CSRF token goes here' });
+});
+
+app.post('/process', function(req, res){
+  console.log('Form (from querystring): ' + req.query.form);
+  console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+  console.log('Name (from visible form field): ' + req.body.name);
+  console.log('Email (from visible form field): ' + req.body.email);
+  res.redirect(303, '/thank-you');
+});
+
+// Thank-you page
+app.get('/thank-you', function(req, res){
+  res.render('thank-you');
 });
 
 // 404 catch-all handler (middleware)
